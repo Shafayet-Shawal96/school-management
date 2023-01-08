828fd81b709b06e7a4402c2e876c967057f5688b
Jobayer Ashique <ashique5299@gmail.com>
2023-01-08 17:45:23 +0600

    wishlist page and single product page partialy done again

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import NavComponent from "./Components/NavComponent";
-
 function App() {
-  return (
-    <div className="App">
-      <NavComponent />
-      {/* <div className="col-lg-8 col-md-12">
-        <div className="footer-menu-social">
-          <div className="footer-menu">
-            <ul>
-              <li>Privecy & Policy</li>
-              <li>Terms & Conditions of Use</li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
