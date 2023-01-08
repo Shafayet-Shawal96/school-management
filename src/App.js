e9d85a44ad9cc7d97353c71165edfe28f696d1fd
Jobayer Ashique <ashique5299@gmail.com>
2023-01-09 03:48:17 +0600

    All pages routing done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import { Route, Routes } from "react-router";
-import FooterLayout from "./Layout/FooterLayout";
-import HeaderLayout from "./Layout/HeaderLayout";
-import IndexPage from "./Pages/IndexPage";
 import AboutUsPage from "./Pages/AboutUsPage";
-import ShopPage from "./Pages/ShopPage";
+import BlogDetailPage from "./Pages/BlogDetailPage";
 import BlogPage from "./Pages/BlogPage";
-import ContactPage from "./Pages/ContactPage";
 import CartPage from "./Pages/CartPage";
 import CheckoutPage from "./Pages/CheckoutPage";
+import ContactPage from "./Pages/ContactPage";
 import CourseDetailsPage from "./Pages/CourseDetailsPage";
 import CoursePage from "./Pages/CoursePage";
-// import EventPage from "./Pages/EventPage ";
+import EventDetailsPage from "./Pages/EventDetailsPage";
+import EventPage from "./Pages/EventPage";
+import IndexPage from "./Pages/IndexPage";
 import IndexPageThree from "./Pages/IndexPageThree";
 import IndexPageTwo from "./Pages/IndexPageTwo";
 import LoginRegisterPage from "./Pages/LoginRegisterPage";
+import ShopPage from "./Pages/ShopPage";
 import SingleProductPage from "./Pages/SingleProductPage";
 import WishlistPage from "./Pages/WishlistPage";
-import EventPage from "./Pages/EventPage";
-// import WishlistPage from "./Pages/WishlistPage";
 
 function App() {
   return (
     <div>
-      <EvantDeatilsPage />
+      <Routes>
+        <Route path="about" element={<AboutUsPage />} />
+        <Route path="blog-detail" element={<BlogDetailPage />} />
+        <Route path="blog" element={<BlogPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="course-detail" element={<CourseDetailsPage />} />
+        <Route path="course" element={<CoursePage />} />
+        <Route path="about" element={<EventDetailsPage />} />
+        <Route path="event" element={<EventPage />} />
+        <Route path="index" element={<IndexPage />} />
+        <Route path="index-three" element={<IndexPageThree />} />
+        <Route path="index-two" element={<IndexPageTwo />} />
+        <Route path="login-register" element={<LoginRegisterPage />} />
+        <Route path="shop" element={<ShopPage />} />
+        <Route path="single-product" element={<SingleProductPage />} />
+        <Route path="wishlist" element={<WishlistPage />} />
+      </Routes>
     </div>
   );
 }
